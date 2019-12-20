6756c92ead71d5ad6e4572c846f8e11715dd8f89
yc <3095220780@qq.com>
2019-12-20 16:10:19 +0800

    :lipstick: 芯片绑定样式调整

--- lib/pages/chip_bind_detail_page/index.dart
+++ lib/pages/chip_bind_detail_page/index.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../../components/myExpansionTile.dart';
+import '../../components/search_bar.dart';
 
 class ChipBindDetailPage extends StatefulWidget {
   ChipBindDetailPage({Key key}) : super(key: key);
@@ -25,10 +27,19 @@ class _ChipBindDetailPageState extends State<ChipBindDetailPage> {
   Widget build(BuildContext context) {
     ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     return Scaffold(
-      appBar: AppBar(title: Text('设备信息'),),
+      appBar: AppBar(
+        title: Text('设备信息'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: (){},
+          )
+        ],
+      ),
       backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
       body: ListView(
         children: [
+          SearchBarDemo(),
           MyExpansionTile(
             onTap: (){
               setState(() {
@@ -36,11 +47,47 @@ class _ChipBindDetailPageState extends State<ChipBindDetailPage> {
               });
             },
             isExpanded: isActive(0),
-            title:Text('好友'),
-            children: <Widget>[
-              ListTile(title: Text('我的好友1')),
-              ListTile(title: Text('我的好友1'))
-            ],
+            title:Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('南方泵业宿舍1号水泵',style: TextStyle(
+                  fontSize: ScreenUtil().setSp(30)
+                ),),
+                Text('AATJ00018000810000000087',style: TextStyle(
+                  fontSize: ScreenUtil().setSp(24),
+                  color: Colors.grey
+                ),)
+              ],
+            ),
+            children: List.generate(5, (i)=>(
+              Slidable(
+                actionPane: SlidableScrollActionPane(),//滑出选项的面板 动画
+                actionExtentRatio: 0.25,
+                child: ListTile(
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset('assets/images/chip.png',width: 56,height: 55),
+                  ),
+                  title: Text('DSFSDSGDSGSDF$i'),
+                  subtitle: Text('前轴承'),
+                ),
+                secondaryActions: <Widget>[//右侧按钮列表
+                  IconSlideAction(
+                    caption: 'More',
+                    color: Colors.black45,
+                    icon: Icons.more_horiz,
+                    onTap: (){},
+                  ),
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    closeOnTap: false,
+                    onTap: (){},
+                  ),
+                ],
+              )
+            )),
           ),
           MyExpansionTile(
             onTap: (){
@@ -49,17 +96,47 @@ class _ChipBindDetailPageState extends State<ChipBindDetailPage> {
               });
             },
             isExpanded: isActive(1),
-            title:Text('家人'),
-            children: <Widget>[
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-              ListTile(title: Text('我的家人1')),
-            ],
+            title:Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('南方泵业宿舍2号水泵',style: TextStyle(
+                  fontSize: ScreenUtil().setSp(30)
+                ),),
+                Text('AATJ000180008100002323087',style: TextStyle(
+                  fontSize: ScreenUtil().setSp(24),
+                  color: Colors.grey
+                ),)
+              ],
+            ),
+            children: List.generate(5, (i)=>(
+              Slidable(
+                actionPane: SlidableScrollActionPane(),//滑出选项的面板 动画
+                actionExtentRatio: 0.25,
+                child: ListTile(
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset('assets/images/chip.png',width: 56,height: 55),
+                  ),
+                  title: Text('DSFSDSGDSGSDF$i'),
+                  subtitle: Text('后轴承'),
+                ),
+                secondaryActions: <Widget>[//右侧按钮列表
+                  IconSlideAction(
+                    caption: 'More',
+                    color: Colors.black45,
+                    icon: Icons.more_horiz,
+                    onTap: (){},
+                  ),
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    closeOnTap: false,
+                    onTap: (){},
+                  ),
+                ],
+              )
+            ))
           ),
           MyExpansionTile(
             onTap: (){
@@ -68,13 +145,47 @@ class _ChipBindDetailPageState extends State<ChipBindDetailPage> {
               });
             },
             isExpanded: isActive(2),
-            title:Text('朋友'),
-            children: <Widget>[
-              ListTile(title: Text('我的朋友1')),
-              ListTile(title: Text('我的朋友1')),
-              ListTile(title: Text('我的朋友1')),
-              ListTile(title: Text('我的朋友1'))
-            ],
+            title:Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('南方泵业宿舍3号水泵',style: TextStyle(
+                  fontSize: ScreenUtil().setSp(30)
+                ),),
+                Text('AATJ000180008100002323087',style: TextStyle(
+                  fontSize: ScreenUtil().setSp(24),
+                  color: Colors.grey
+                ),)
+              ],
+            ),
+            children: List.generate(5, (i)=>(
+              Slidable(
+                actionPane: SlidableScrollActionPane(),//滑出选项的面板 动画
+                actionExtentRatio: 0.25,
+                child: ListTile(
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset('assets/images/chip.png',width: 56,height: 55),
+                  ),
+                  title: Text('DSFSDSGDSGSDF$i'),
+                  subtitle: Text('左侧面'),
+                ),
+                secondaryActions: <Widget>[//右侧按钮列表
+                  IconSlideAction(
+                    caption: 'More',
+                    color: Colors.black45,
+                    icon: Icons.more_horiz,
+                    onTap: (){},
+                  ),
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    closeOnTap: false,
+                    onTap: (){},
+                  ),
+                ],
+              )
+            ))
           ),
         ],
       ),
@@ -82,3 +193,24 @@ class _ChipBindDetailPageState extends State<ChipBindDetailPage> {
   }
 }
 
+
+class SearchBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 52.0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: new RaisedButton.icon(
+          label: Text('搜索',style: TextStyle(color: Colors.grey),),
+          icon: Icon(Icons.search,color: Colors.grey,),
+          onPressed: (){
+            showSearch(context: context, delegate: SearchBarDelegate());
+          },
+          color: Colors.white,
+          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        )
+      ),
+    );
+  }
+}
